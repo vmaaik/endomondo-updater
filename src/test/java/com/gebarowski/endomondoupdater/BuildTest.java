6a78ed43db7911df118f823a52e741e0ac84f328
michal gebarowski <vmaaik@gmail.com>
2020-07-27 10:27:36 +0100

    Test github actions

---
+++ src/test/java/com/gebarowski/endomondoupdater/BuildTest.java
@@ -0,0 +1,21 @@
+package com.gebarowski.endomondoupdater;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class BuildTest {
+
+    @Test
+    public void test1() {
+        int test = 1;
+        Assert.assertEquals(test, 1);
+    }
+
+
+    @Test
+    public void test2() {
+        int test = 2;
+        Assert.assertEquals(test, 1);
+    }
+}
